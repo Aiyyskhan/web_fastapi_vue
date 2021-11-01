61373a6d3ee38d1650b8187e3f40210fcddf14ef
Aiyyskhan <aiyyskhan@gmail.com>
2021-11-01 14:20:34 +0900

    changed main files

---
+++ front/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Ping',
+    data() {
+      return {
+        msg: '',
+      };
+    },
+    methods: {
+      getMessage() {
+        axios.get('http://127.0.0.1:8000')
+          .then((res) => {
+            this.msg = res.data;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      },
+    },
+    created() {
+      this.getMessage();
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
