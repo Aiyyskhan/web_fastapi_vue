b030b2a64e03d7f9d588a70bae04b8772f04dc82
Aiyyskhan <aiyyskhan@gmail.com>
2021-11-05 16:20:06 +0900

    modified index.html, App.vue

--- services/front/src/App.vue
+++ services/front/src/App.vue
@@ -7,19 +7,12 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import NavBar from '@/components/NavBar.vue'
-export default {
-  components: {
-    NavBar
-  }
-}
-</script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -28,3 +21,15 @@ export default {
   padding-top: 5em;
 }
 </style>
+
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
--- services/front/src/components/NavBar.vue
+++ services/front/src/components/NavBar.vue
@@ -2,11 +2,15 @@
   <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container">
+
         <a class="navbar-brand" href="/">Altan</a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarCollapse">
+          
           <ul v-if="isLoggedIn" class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
               <router-link class="nav-link" to="/">Home</router-link>
@@ -21,6 +25,7 @@
               <a class="nav-link" @click="logout">Log Out</a>
             </li>
           </ul>
+
           <ul v-else class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
               <router-link class="nav-link" to="/">Home</router-link>
